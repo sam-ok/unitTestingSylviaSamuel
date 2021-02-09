aa5321dc37b2477c217f8ff6bc12b99c25655d55
Sam O <sam.ictdev@gmail.com>
2021-02-09 23:28:58 +0300

    Update factorialTest.js

--- test/factorialTest.js
+++ test/factorialTest.js
@@ -8,4 +8,21 @@ describe('Factorial Test', function(){
         let result = factorial(1);
         assert.equal(result, '1');
     })
+    it('Return factorial of a number', function(){
+        let result = factorial(2);
+        assert.equal(result, '2');
+    })
+    it('Return factorial of a number', function(){
+        let result = factorial(3);
+        assert.equal(result, '6');
+    })
+    it('Return factorial of a number', function(){
+        let result = factorial(0);
+        assert.equal(result, '1');
+    })
+    it('Return factorial of a number', function(){
+        let result = factorial(10);
+        assert.equal(result, '3628800');
+    })
+
 })
